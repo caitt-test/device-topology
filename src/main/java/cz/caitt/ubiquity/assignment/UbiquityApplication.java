cc52359b2c8d68df021ad758c6c25227250f5fd4
Tomas Caithaml <caitt@users.sourceforge.net>
2024-12-26 22:51:09 +0100

    Init commit

---
+++ src/main/java/cz/caitt/ubiquity/assignment/UbiquityApplication.java
@@ -0,0 +1,13 @@
+package cz.caitt.ubiquity.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiquityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbiquityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/caitt/ubiquity/assignment/UbiquityApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.caitt.ubiquity.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbiquityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
